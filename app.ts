a03ec4f1bedf4d1261ca9c3c98852a7a6b2db194
Tariq-Monowar <hassainseam@gmail.com>
2025-01-31 16:00:57 +0600

    add

--- app.ts
+++ app.ts
@@ -1,15 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import morgan from "morgan";
-import session from 'express-session';
+import session from "express-session";
 
 import users from "./module/users/users.routes";
 import path from "path";
-// import battery from "./module/Battery/battery.routes";
+import battery from "./module/battery/battery.routes";
 import warranty from "./module/warranty/warranty.routes";
 import modelNumber from "./module/model-number/modelNumber.routes";
 import notificationRoutes from "./module/notification/notification.routes";
-
 
 const app = express();
 
@@ -23,7 +22,7 @@ app.use(
       "http://localhost:58626",
       "http://localhost:61801",
       "http://192.168.40.47:3000",
-      "http://192.168.40.47:*"
+      "http://192.168.40.47:*",
     ],
   })
 );
@@ -31,25 +30,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'your-secret-key',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // app.use(passport.initialize());
 // app.use(passport.session());
 
 app.use("/users", users);
-// app.use("/battery", battery)
-app.use("/warranty", warranty)
-app.use("/model-number", modelNumber)
+app.use("/battery", battery);
+app.use("/warranty", warranty);
+app.use("/model-number", modelNumber);
 app.use("/notification", notificationRoutes);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-console.log("json", process.cwd() + "\\firebase-service-account.json")
-
+console.log("json", process.cwd() + "\\firebase-service-account.json");
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(404).json({
